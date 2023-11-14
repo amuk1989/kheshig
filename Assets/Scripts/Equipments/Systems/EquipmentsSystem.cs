f58f39ce1271f7ce5e8613087c63934f7db0c4a6
Andrey Muholzoev <amuk89@mail.ru>
2023-11-14 21:56:57 +0700

    rename

---
+++ Assets/Scripts/Equipments/Systems/EquipmentsSystem.cs
@@ -0,0 +1,12 @@
+using Unity.Entities;
+
+namespace Equipments.Systems
+{
+    public partial struct EquipmentsSystem: ISystem
+    {
+        public void OnUpdate(ref SystemState state)
+        {
+            
+        }
+    }
+}
